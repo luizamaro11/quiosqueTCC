a29a8352a19e67ee36d0e080aa1138c9447f968e
luizamaro11 <luiz.amaro90@gmail.com>
2019-09-22 04:31:19 +0900

    criando autenticação em login com jQuery.

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,5 +1,25 @@
 /**/
+$(document).ready(function(){
 
- $("#logar").click(function(){
-     
- });
+    /*autenticação do login para entrar apenas se o usuario e senha for admin e exibir mensagem caso não for*/ 
+    $(document).on("click", "#logar", function(){
+        var usuario = $("#usuario").val();
+        var senha = $("#senha").val();
+
+        if(usuario == "admin" && senha == "admin"){
+            $(location).attr("href", "menu.html");
+        } else if(usuario != "admin" && senha != "admin"){
+            $("#mensagemUsuario").html("usuario está incorreto, por favor tentar novamente");
+            $("#mensagemUsuario").css("color", "red");
+
+            $("#mensagemSenha").html("senha está incorreto, por favor tentar novamente");
+            $("#mensagemSenha").css("color", "red");
+        } else if(usuario != "admin"){
+            $("#mensagemUsuario").html("usuario está incorreto, por favor tentar novamente");
+            $("#mensagemUsuario").css("color", "red");
+        } else if(senha != "admin"){
+            $("#mensagemSenha").html("senha está incorreto, por favor tentar novamente");
+            $("#mensagemSenha").css("color", "red");
+        }  
+    });
+});
